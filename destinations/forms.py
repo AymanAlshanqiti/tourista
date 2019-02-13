72a625572a0bc39a608772373d6d16ef4bca8f5b
Ayman Alshanqiti <ayman.alshanqiti@gmail.com>
2019-02-14 01:36:57 +0300

    Add some styleing to the script

--- destinations/forms.py
+++ destinations/forms.py
@@ -6,3 +6,11 @@ class DestinationForm(forms.ModelForm):
     class Meta:
         model = Destination
         fields = '__all__'
+
+
+
+    # def __init__(self, *args, **kwargs):
+    # 	super().__init__(*args, **kwargs)
+    # 	self.helper = FormHelper()
+
+
